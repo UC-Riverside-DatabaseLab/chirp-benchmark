b3ce17dae70506d6d326a32c4089c6848f7ec9b9
Abhinand Menon <abhinandmenon@gmail.com>
2014-10-28 19:55:20 -0700

    v0.2 - Added readme, example and license.

--- benchmark_gen.cc
+++ benchmark_gen.cc
@@ -8,7 +8,8 @@
 using namespace std;
 
 
-int create_extract(int lines_to_read = 50, string in_file = "/home/abhinand/work/all.csv", string out_file = "tweet_extract.txt") {
+// Create a extract containing fewer tweets
+int create_extract(int lines_to_read = 50, string in_file = "input_file.txt", string out_file = "tweet_extract.txt") {
 ifstream ifile(in_file.c_str());
 ofstream ofile(out_file.c_str());
 
@@ -36,6 +37,7 @@ return 0;
 }
 
 
+// Parse out fields required by the benchmark generator
 int parse_tweets(string in_file = "tweet_extract.txt", string out_file = "parsed_tweets.txt") {
 ifstream ifile(in_file.c_str());
 ofstream ofile(out_file.c_str());
@@ -55,38 +57,7 @@ rapidjson::Document d;
 
 while(getline(ifile, line)) {
   d.Parse<0>(line.c_str());
-  ofile << d["CreationTime"].GetInt64() << "@,@" << (d["RetweetsNum"].IsNull() ? 0: d["RetweetsNum"].GetInt()) << "@,@" << d["UserID"].GetInt64() << "@,@" << d["ID"].GetInt64() << "@,@" << line << endl;
-}
-
-ifile.close();
-ofile.close();
-
-return 0;
-}
-
-
-int generate(string in_file = "tweet_extract.txt", string out_file = "benchmark.txt") {
-ifstream ifile(in_file.c_str());
-ofstream ofile(out_file.c_str());
-
-if (!ifile) {
-  cerr << "Can't open input file " << endl;
-  return -1;
-}
-
-if (!ofile) {
-  cerr << "Can't open output file " << endl;
-  return -2;
-}
-
-string line;
-rapidjson::Document d;
-
-while(getline(ifile, line)) {
-  d.Parse<0>(line.c_str());
-
-  int64_t sortval[2] = {d["CreationTime"].GetInt64(), d["ID"].GetInt64()};
-  ofile << sortval[0] << " " << sortval[1] << " " << line << endl;
+  ofile << d["CreationTime"].GetUInt64() << "@,@" << (d["RetweetsNum"].IsNull() ? 0: d["RetweetsNum"].GetUInt()) << "@,@" << d["UserID"].GetUInt64() << "@,@" << d["ID"].GetUInt64() << "@,@" << line << endl;
 }
 
 ifile.close();
@@ -98,7 +69,7 @@ return 0;
 
 int main () {
 
-create_extract(5000000);
+create_extract(5000000); // Extract 5 million tweets from input file
 parse_tweets();
 
 return 0;
