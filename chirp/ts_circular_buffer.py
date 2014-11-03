7a503a4ac7fb7bb0aab4efbb0f24070684549dff
Abhinand Menon <abhinandmenon@gmail.com>
2014-11-03 13:50:49 -0800

    Fix in TSCircularBuffer to prevent math range error.

--- chirp/ts_circular_buffer.py
+++ chirp/ts_circular_buffer.py
@@ -14,17 +14,18 @@ class TSCircularBuffer():
         self._size = size
         self._cursor = 0
         self._rolled_over_once = False
-        self._zero = 0
+        self._prob_zero = 0
+        self._exp_zero = 0
     
     
     def insert(self, item, log_prob):
         if self._rolled_over_once:
-            self._zero = self._cum_prob[self._cursor]
+            self._prob_zero = self._cum_prob[self._cursor]
         
         self._data[self._cursor] = item
         
         if self._rolled_over_once:
-            self._cum_prob[self._cursor] = self._cum_prob[self._cursor-1] + math.exp(log_prob)
+            self._cum_prob[self._cursor] = self._cum_prob[self._cursor-1] + math.exp(log_prob - self._exp_zero)
         elif self._cursor != 0:
             self._cum_prob.append(self._cum_prob[self._cursor-1] + math.exp(log_prob))
         else:
@@ -34,13 +35,23 @@ class TSCircularBuffer():
         if self._cursor == self._size:
             self._rolled_over_once = True
             self._cursor = 0
+            self._adjustSums()
+    
+    
+    def _adjustSums(self):
+        ratio = self._cum_prob[-1] - self._cum_prob[-2]
+        self._exp_zero = math.log(ratio)
+        
+        for index in xrange(self._size):
+            self._cum_prob[index] /= ratio
+        self._prob_zero /= ratio
     
     
     def rand(self):
         if not self._rolled_over_once and self._cursor == 0:
             return None
         
-        threshold = random.random() * (self._cum_prob[self._cursor - 1] - self._zero) + self._zero
+        threshold = random.random() * (self._cum_prob[self._cursor - 1] - self._prob_zero) + self._prob_zero
         
         if self._rolled_over_once:
             return self._thresholdItem(threshold, self._cursor - self._size, self._cursor - 1 )
