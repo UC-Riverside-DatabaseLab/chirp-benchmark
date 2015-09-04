6a1b1bf67ba76021ad1931b551959b0794ca1b29
Abhinand Menon <abhinandmenon@gmail.com>
2015-09-04 16:20:02 -0700

    updated example script

--- example/run_me.sh
+++ example/run_me.sh
@@ -4,8 +4,8 @@ echo
 echo "Folder contents: "
 ls
 echo
-echo "Running command ../chirp.py -i tweet_sample.txt"
-../run_chirp.py -i data/tweet_extract.txt
+echo -e "Running command: \033[32m../run_chirp.py -i tweet_sample.txt\033[0m"
+../run_chirp.py -i tweet_sample.txt
 echo "Benchmark created"
 echo
 echo "Folder contents: "
